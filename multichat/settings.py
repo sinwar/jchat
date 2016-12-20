ddb438a1358ccb76e82fcbf14f2fcc8706d958b7
sinwar <premsinwar4@gmail.com>
2016-12-21 04:17:47 +0530

    glitch removed

--- multichat/settings.py
+++ multichat/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
-STATIC_ROOT = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "pr")
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
