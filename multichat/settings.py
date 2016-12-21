2019e506908345638d9f795211c0f1aa6f83f4a7
sinwar <premsinwar4@gmail.com>
2016-12-22 00:58:18 +0530

    glitch removed from settings.py

--- multichat/settings.py
+++ multichat/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'O),
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 ACCOUNT_ACTIVATION_DAYS = 7
